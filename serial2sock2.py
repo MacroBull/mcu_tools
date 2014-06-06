0a616430342c8d7e94638409ab3821844ca0a9b4
Macrobull <MacrobullG@Gmail.com>
2014-06-06 22:58:00 +0800

    *bug fix

--- serial2sock2.py
+++ serial2sock2.py
@@ -19,38 +19,13 @@ ser.flushInput()
 ser.flushOutput()
 ser.close()
 
-baud = 9600
-intv = 0.05
+baud = 460800
+intv = 0.1
+timeOut = 1
 port = 8888
 
-#try:
-#	ser = serial.Serial(dev, baud, timeout=intv)
-#	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#	sock.bind(('', port))
-#	sock.settimeout(sockTOut)
-#	print "dev={} baudrate={} port={}".format(dev, ser.getBaudrate(), port)
-#
-#	conn = listenSock(sock)
-#
-#	while 1:
-#		sockData = serData = ''
-#		status = 1
-#		try:
-#			while ser.inWaiting():
-#				serData += ser.read(1)
-#			sockData = conn.recv(1024)
-#			if sockData: status = 0
-#		except BaseException:
-#			status = 2
-#
-#		#print "{}:{}:{},{}".format(time.time(), status, serData, sockData)
-#		print "{}:{}:{},{}".format(conn.getpeername(), status, serData, sockData)
-#		if status == 1:
-#			conn = listenSock(sock)
-#
-#		ser.write(sockData)
-#		conn.sendall(serData)
-
+ser = serial.Serial(dev, baud, timeout=intv)
+print "dev={} baudrate={} port={}".format(dev, ser.getBaudrate(), port)
 
 class s2s(Protocol):
 	def __exit__(self):
@@ -61,12 +36,27 @@ class s2s(Protocol):
 		if not ser.isOpen(): ser.open()
 
 		ser.write(sockData)
-#		while not serData:
-#			while ser.inWaiting():
-#				serData += ser.read(10)
-		serData = ser.read(1024)
-		serData += ser.read(ser.inWaiting())
 
+
+		time.sleep(intv)
+		serData = ' ' + ser.read(1024)
+		t = timeOut
+		while serData[-1]!='\n':
+			t -=intv
+			time.sleep(intv)
+			serData += ser.read(ser.inWaiting())
+			if t<0: break
+#		serData = ser.read(1024)
+#		i = timeOut
+#		while i>0:
+#			#if ser.inWaiting():
+#			if serData[-1]!='\n':
+#				serData += ser.read(ser.inWaiting())
+#				i = timeOut
+#			i -= intv
+#			time.sleep(intv)
+
+		serData = serData[1:]
 		self.transport.write(serData)
 
 		print "====Input====\n{}\n\n====Output====\n{}\n\n".format(sockData, serData)
@@ -74,8 +64,6 @@ class s2s(Protocol):
 
 	def connectionMade(self):
 		print "Connected"
-		ser = serial.Serial(dev, baud, timeout=intv)
-		print "dev={} baudrate={} port={}".format(dev, ser.getBaudrate(), port)
 
 	def connectionLost(self, reason):
 		if not ser.isOpen():
